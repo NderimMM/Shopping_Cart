909ab776f475b70b1425db974f185a55ce8e905c
BardhKorca <bardh.korqa@gmail.com>
2021-06-01 17:35:58 +0200

    v3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import Product from './components/Product';
-import data from './data';
+import {BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
       <header className="row">
         <div>
@@ -15,19 +18,13 @@ function App() {
         </div>
       </header>
       <main>
-        <div>
-          <div className="row center">
-            {
-              data.products.map(product => (
-                <Product key={product._id} product = {product}></Product>
-              ))
-            }
-          </div>
-        </div>
+      <Route path="/product/:id " component={ProductScreen}></Route>
+      <Route path="/" component={HomeScreen} exact></Route>
+
       </main>
       <footer className="row center">All right reserved</footer>
     </div>
-
+    </BrowserRouter>
   );
 }
 
