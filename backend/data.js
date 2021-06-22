8bf6c63916fd3376cd3393d4fab92290616bcacc
unknown <nm43080@ubt-uni.net>
2021-06-22 11:43:32 +0200

    lidhja me databaz fillimi

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name:'Nderim',
+      email:'nderim.steam321@gmail.com',
+      password:bcrypt.hashSync('1234',8),
+      isAdmin:true,
+    },
+
+    {
+      name:'Besar',
+      email:'besartasholli@gmail.com',
+      password:bcrypt.hashSync('1234',8),
+      isAdmin:false,
+    }
+  ],
     products: [
       {
         _id: '1',
